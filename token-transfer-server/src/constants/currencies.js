7f1952995a7ed83de879e904fc856fab55181529
Tom Linton <tlinton@fastmail.fm>
2022-05-26 11:32:26 +1200

    Add constants file defining supported tokens

---
+++ token-transfer-server/src/constants/currencies.js
@@ -0,0 +1,10 @@
+const currencies = {
+  OGN: {
+    mainnet: '0x8207c1ffc5b6804f6024322ccf34f29c3541ae26',
+    rinkeby: '0xa115e16ef6e217f7a327a57031f75ce0487aadb8',
+  },
+  OGV: {
+    mainnet: '',
+    rinkeby: '',
+  },
+}
